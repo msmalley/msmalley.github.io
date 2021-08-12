6305df5d3914d832d2102d438565b047e7ff303c
Mark Smalley <mark@smalley.my>
2021-08-12 17:11:45 +0800

    minor tweaks

--- gallery/js/home.js
+++ gallery/js/home.js
@@ -45,96 +45,84 @@ function getArtistDescription(title, surname, colours, callback)
     var gender_term = 'their';
     var gender_termed = 'It';
     var name = title + ' ' + surname;
-    const digest = async ({ algorithm = "SHA-256", message }) =>
-      Array.prototype.map
-        .call(
-          new Uint8Array(
-            await crypto.subtle.digest(algorithm, new TextEncoder().encode(message))
-          ),
-          (x) => ("0" + x.toString(16)).slice(-2)
-        )
-        .join("");
+    var id = EthJS.Util.sha256(name).toString('hex');
+    var seed = stringToSeed(id);
+    if(
+        title == 'Daddy'
+        || title == 'King'
+        || title == 'Master'
+        || title == 'Mr.'
+        || title == 'Papa'
+        || title == 'Prince'
+        || title == 'Sir'
+    ){
+        gender = 'male';
+        gender_term = 'his';
+        gender_termed = 'He';
+    }
+    else if(
+        title == 'Granny'
+        || title == 'Madam'
+        || title == 'Mama'
+        || title == 'Mistress'
+        || title == 'Mrs.'
+        || title == 'Ms'
+        || title == 'Queen'
+    ){
+        gender = 'female';
+        gender_term = 'her';
+        gender_termed = 'She';
+    }
+    var job = getRelevantRandomWord('noun', 'job', false, seed);
+    var firstname = getRelevantRandomWord('firstname', gender, false, seed);
+    var time = getRelevantRandomWord('timeadv', 'past', false, seed);
+    var relative = getRelevantRandomWord('rel', 'all', false, seed);
 
-    digest({message: name}).then(function(id)
+    var random = new XorShift128(seed);
+
+    var commitments = [
+        'is a professional',
+        'is an amateur',
+        'is a retired',
+        'is an ex',
+    ];
+    var commitment_index = random.integer(0, (commitments.length - 1));
+
+    var however = ' who is now working on';
+    if(commitment_index < 2) however = ', but would rather work on';
+
+    var colour1 = colours[0].name.toLowerCase();
+    var colour2 = colours[1].name.toLowerCase();
+    var colour3 = colours[2].name.toLowerCase();
+    var number_of_children = random.integer(0, 3);
+    var commitment_type = commitments[commitment_index];
+    var child_status_intro = 'Although it';
+    if(gender == 'male') child_status_intro = 'Athough he';
+    else if(gender == 'female') child_status_intro = 'Athough she';
+
+    var child_job = getRelevantRandomWord('noun', 'job', false, parseInt(reverseString('' + number_of_children + seed + '')));
+
+    var child_status = child_status_intro + ' has no ' + colour3 + ' children, ' + firstname + ' plans to marry'; // 51 * 
+
+    var conclusions = ' to ' + gender_term + ' ' + relative + '\'s favourite ' + child_job + '.';
+
+    if(number_of_children == 1)
     {
-        var seed = stringToSeed(id);
-        if(
-            title == 'Daddy'
-            || title == 'King'
-            || title == 'Master'
-            || title == 'Mr.'
-            || title == 'Papa'
-            || title == 'Prince'
-            || title == 'Sir'
-        ){
-            gender = 'male';
-            gender_term = 'his';
-            gender_termed = 'He';
-        }
-        else if(
-            title == 'Granny'
-            || title == 'Madam'
-            || title == 'Mama'
-            || title == 'Mistress'
-            || title == 'Mrs.'
-            || title == 'Ms'
-            || title == 'Queen'
-        ){
-            gender = 'female';
-            gender_term = 'her';
-            gender_termed = 'She';
-        }
-        var job = getRelevantRandomWord('noun', 'job', false, seed);
-        var firstname = getRelevantRandomWord('firstname', gender, false, seed);
-        var time = getRelevantRandomWord('timeadv', 'past', false, seed);
-        var relative = getRelevantRandomWord('rel', 'all', false, seed);
-        
-        var random = new XorShift128(seed);
-        
-        var commitments = [
-            'is a professional',
-            'is an amateur',
-            'is a retired',
-            'is an ex',
-        ];
-        var commitment_index = random.integer(0, (commitments.length - 1));
-        
-        var however = ' who is now working on';
-        if(commitment_index < 2) however = ', but would rather work on';
-        
-        var colour1 = colours[0].name.toLowerCase();
-        var colour2 = colours[1].name.toLowerCase();
-        var colour3 = colours[2].name.toLowerCase();
-        var number_of_children = random.integer(0, 3);
-        var commitment_type = commitments[commitment_index];
-        var child_status_intro = 'Although it';
-        if(gender == 'male') child_status_intro = 'Athough he';
-        else if(gender == 'female') child_status_intro = 'Athough she';
-        
-        var child_job = getRelevantRandomWord('noun', 'job', false, parseInt(reverseString('' + number_of_children + seed + '')));
-        
-        var child_status = child_status_intro + ' has no ' + colour3 + ' children, ' + firstname + ' plans to marry'; // 51 * 
-        
-        var conclusions = ' to ' + gender_term + ' ' + relative + '\'s favourite ' + child_job + '.';
-        
-        if(number_of_children == 1)
-        {
-            child_status = 'Married ' + time + ' and having recently given birth to ' + gender_term + ' first ' + colour3 + ' child';
-            conclusions = '; ' + firstname + ' is eager for change.';
-        }
-        else if(number_of_children > 2)
-        {
-            
-            var verbage = 'a';
-            if(startsWithVowel(colour3)) verbage = 'an';
-            
-            child_status = 'Married with ' + number_of_children + ' children; one of which is eager to become ' + verbage + ' ' + colour3 + ' ' + child_job;
-            conclusions = ' like ' + firstname + '\'s ' + relative +' was.';
-        }
-        
-        var description = firstname + ' ' + surname + ' ' + commitment_type + ' ' + colour1 + ' ' + job + '' + however + ' ' + gender_term + ' ' + colour2 + ' art instead. ' + child_status + '' + conclusions;
-        callback(description);
-    });
+        child_status = 'Married ' + time + ' and having recently given birth to ' + gender_term + ' first ' + colour3 + ' child';
+        conclusions = '; ' + firstname + ' is eager for change.';
+    }
+    else if(number_of_children > 2)
+    {
+
+        var verbage = 'a';
+        if(startsWithVowel(colour3)) verbage = 'an';
+
+        child_status = 'Married with ' + number_of_children + ' children; one of which is eager to become ' + verbage + ' ' + colour3 + ' ' + child_job;
+        conclusions = ' like ' + firstname + '\'s ' + relative +' was.';
+    }
+
+    var description = firstname + ' ' + surname + ' ' + commitment_type + ' ' + colour1 + ' ' + job + '' + however + ' ' + gender_term + ' ' + colour2 + ' art instead. ' + child_status + '' + conclusions;
+    callback(description);
 }
 
 function getImageColours(img)
