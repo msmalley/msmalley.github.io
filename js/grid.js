1d40837561e45bdb64bd0e83559183943bfdb974
Mark Smalley <mark@smalley.my>
2024-06-04 10:40:56 +0800

    update smalley new wip

--- js/grid.js
+++ js/grid.js
@@ -6,8 +6,6 @@ DEPENDENCIES
 
 */
 
-var smalley_intervals = [];
-
 var smalley =
 {
     init: function()
@@ -18,9 +16,30 @@ var smalley =
     isotope: function()
     {
         var iso = new Isotope('.row');
-        //container.appendChild( elem );
-        //iso.appended( elem );
         iso.layout();
+        
+        var grid_items = document.querySelectorAll('.grid');
+        	
+        grid_items.forEach(function(grid_item, index)
+        {
+            grid_item.addEventListener("click", async function(e)
+            {
+                grid_item.classList.add('animate');
+                if(grid_item.classList.contains('full-width'))
+                {
+                    grid_item.classList.remove('full-width');
+                }
+                else
+                {
+                    grid_item.classList.add('full-width');
+                }
+                setTimeout(function()
+                {
+                    grid_item.classList.remove('animate');
+                    iso.layout();
+                }, 600);
+            });
+        });
     }
 }
 
